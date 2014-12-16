2e2178764641160b281dbe864c95cf0545da622c
arie rachmad syulistyo <rie.momorie@gmail.com>
2014-12-16 17:41:00 +0700

    add contoh picasso di whatsHot

--- src/com/ui/asynctask/tambahKosAsynctask.java
+++ src/com/ui/asynctask/tambahKosAsynctask.java
@@ -1,35 +1,5 @@
 package com.ui.asynctask;
 
-import android.os.AsyncTask;
+public class tambahKosAsynctask {
 
-public class tambahKosAsynctask extends AsyncTask<String, Void, String>{
-
-	@Override
-	protected String doInBackground(String... params) {
-//		try {
-//			JSONObject json = new JSONObject();
-//			json.put("nit", mAddKematianModelSync.getNit());
-//			json.put("tanggal_kematian", mAddKematianModelSync.getTanggal_kematian());
-//			json.put("sebab_kematian", mAddKematianModelSync.getSebab_kematian());
-//			json.put("idl_kematian", mAddKematianModelSync.getIdl_kematian());
-//			json.put("id_petugas", mAddKematianModelSync.getId_petugas());
-//			json.put("user", mAddKematianModelSync.getUser());
-//			json.put("pass", mAddKematianModelSync.getPass());
-//			json.put("guid", mAddKematianModelSync.getGuid());
-//			json.put("transaksi",
-//					mContext.getResources().getString(
-//							R.string.add_data_kematian));
-//
-//			Log.d("json", json.toString());
-//
-//			return HttpRequestHelper.doPost(
-//					mContext.getResources().getString(
-//							R.string.url_insert_data_kematian), json);
-
-//		} catch (JSONException e) {
-//			e.printStackTrace();
-//		}
-		return null;
-	}
-	
 }
--- src/com/ui/kosanku/WhatsHotFragment.java
+++ src/com/ui/kosanku/WhatsHotFragment.java
@@ -20,3 +20,217 @@ public class WhatsHotFragment extends Fragment {
         return rootView;
     }
 }
+
+/*
+ package com.example.akmalgafar.httpsampleapp;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.Header;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.net.URI;
+
+public class AddEmployeeActivity extends Activity {
+    private EditText edtName,edtEmail,edtPhone;
+    private Button btnSelectPhoto,btnSubmit;
+    private ImageView imgPhoto;
+    public static int PICK_PHOTO = 100;
+
+    private String SelectedImagePath ="";
+    private String url = "http://maf.ui.ac.id/services/add_pegawai.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        getActionBar().setTitle("Create New");
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        edtEmail = (EditText) findViewById(R.id.edtAddEmail);
+        edtName = (EditText) findViewById(R.id.edtAddName);
+        edtPhone = (EditText) findViewById(R.id.edtAddPhone);
+
+        btnSelectPhoto = (Button) findViewById(R.id.btnAddSelectPhoto);
+        btnSubmit = (Button) findViewById(R.id.btnAddNewEmployee);
+        imgPhoto = (ImageView) findViewById(R.id.imgAddEmployeePhoto);
+
+        btnSelectPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_PHOTO);
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                postToServer();
+            }
+        });
+    }
+    protected void postToServer() {
+        final ProgressDialog dialog = new ProgressDialog(AddEmployeeActivity.this);
+        dialog.setTitle("Post Data");
+        dialog.setMessage("Please Wait...");
+        dialog.show();
+
+        String nama = edtName.getText().toString().trim();
+        String email = edtEmail.getText().toString().trim();
+        String phone = edtPhone.getText().toString().trim();
+
+        String path =  SelectedImagePath;
+        Log.d("HTTP_APP", path);
+        File filePath = new File(path);
+
+        RequestParams requestParams = new RequestParams();
+        requestParams.put("nama",nama);
+        requestParams.put("email",email);
+        requestParams.put("phone",phone);
+
+        try {
+            requestParams.put("userfile",filePath);
+
+        } catch (Exception e) {
+
+        }
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.setTimeout(6000);
+        client.post(url,requestParams,new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                dialog.dismiss();
+                fetchResponse(new String(responseBody));
+            }
+
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                   dialog.dismiss();
+                Toast.makeText(AddEmployeeActivity.this, "gagal", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    protected void fetchResponse(String response){
+        try{
+            JSONObject object = new JSONObject(response);
+            if(object.getString("status").equals("ok")){
+                Toast.makeText(AddEmployeeActivity.this,object.getString("message"),Toast.LENGTH_LONG).show();
+            }
+        }
+        catch (Exception e){
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK){
+            Uri selectedImageUri= data.getData();
+
+            SelectedImagePath= Utils.getRealPathFromURI(AddEmployeeActivity.this,selectedImageUri);
+            imgPhoto.setImageURI(selectedImageUri);
+        }
+    }
+
+    public static void toAddEmployeeActivty(Activity activity){
+        activity.startActivityForResult(new Intent(activity,AddEmployeeActivity.class),PICK_PHOTO);
+    }
+} 
+ */
+
+/*
+package com.example.akmalgafar.httpsampleapp;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class EmployeeAdapter extends BaseAdapter {
+
+    ArrayList<Employee> listEmployee;
+    Activity activity;
+
+    public EmployeeAdapter(ArrayList<Employee> listEmployee,Activity activity){
+        this.activity = activity;
+        this.listEmployee = listEmployee;
+    }
+
+    @Override
+    public int getCount() {
+        return listEmployee.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        View view = convertView;
+        ViewHolder holder = null;
+        if(view==null){
+            holder = new ViewHolder();
+            LayoutInflater inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.inf_item_employee,null);
+            holder.imgPhoto = (ImageView)view.findViewById(R.id.imgItemEmployeePhoto);
+            holder.txtName = (TextView)view.findViewById(R.id.txtItemEmployeeName);
+            holder.txtEmail = (TextView)view.findViewById(R.id.txtItemEmployeeEmail);
+            holder.txtPhone = (TextView)view.findViewById(R.id.txtItemEmployeePhone);
+            view.setTag(holder);
+        }else
+        {
+            holder = (ViewHolder) view.getTag();
+        }
+        holder.txtName.setText(listEmployee.get(i).getNama());
+        holder.txtEmail.setText(listEmployee.get(i).getEmail());
+        holder.txtPhone.setText(listEmployee.get(i).getPhone());
+
+        Picasso.with(activity).load(listEmployee.get(i).getPhoto()).error(R.drawable.ic_launcher).into(holder.imgPhoto);
+
+        return view;
+    }
+    static class ViewHolder{
+        ImageView imgPhoto;
+        TextView txtName,txtEmail,txtPhone;
+    }
+}
+ */
