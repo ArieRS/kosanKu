38ffe49a6beb3a1f7dc4e99363b9c8fb9acf55aa
arie rachmad syulistyo <rie.momorie@gmail.com>
2014-12-17 23:03:03 +0700

    add context to all fragment

--- src/com/ui/kosanku/CariKosFragment.java
+++ src/com/ui/kosanku/CariKosFragment.java
@@ -1,21 +1,39 @@
 package com.ui.kosanku;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class CariKosFragment extends Fragment {
 	
-	public CariKosFragment(){}
+	Button buttonCari;
+	Context mContext;
+	public CariKosFragment(Context context){
+		mContext  = context;
+	}
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
  
         View rootView = inflater.inflate(R.layout.fragment_cari_kos, container, false);
-         
+        buttonCari = (Button) rootView.findViewById(R.id.buttonCari);
+		logicControl();
         return rootView;
     }
+	private void logicControl() {
+		buttonCari.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Fragment fragment  = new HasilCariKosFragment(mContext);
+				FragmentManager fragmentManager = getActivity().getFragmentManager();
+				fragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit();
+			}
+		});
+	}
 }
--- src/com/ui/kosanku/HasilCariKosFragment.java
+++ src/com/ui/kosanku/HasilCariKosFragment.java
@@ -1,22 +1,19 @@
 package com.ui.kosanku;
 
-import com.ui.adapter.HasilPencarianKosAdapter;
-import com.ui.common.DatabaseHelper;
-import com.ui.common.General;
-import com.ui.common.SessionManager;
-import com.ui.kosanku.R;
-
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.Fragment.SavedState;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+
+import com.ui.common.DatabaseHelper;
+import com.ui.common.SessionManager;
 
 public class HasilCariKosFragment extends Fragment {
 	Context mContext;
@@ -25,31 +22,46 @@ public class HasilCariKosFragment extends Fragment {
 	ListView listView;
 	DatabaseHelper db;
 	SessionManager sessionManager;
-
+	
 	public HasilCariKosFragment(Context context) {
 		mContext = context;
-		db = new DatabaseHelper(mContext);
-		sessionManager = new SessionManager(mContext);
 	}
-
+	
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		activity = (Activity) mContext;
-		 actioncBar = activity.getActionBar();
-		// actioncBar.setHomeButtonEnabled(true);
-		 actioncBar.setDisplayHomeAsUpEnabled(true);
-		// setHasOptionsMenu(true);
-
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+		View rootView = inflater.inflate( R.layout.list_view_fragment_cari_kos, container, false);
+		listView = (ListView) rootView.findViewById(R.id.ListFragmentHasilPencarian);
+		logicControl();
+		return rootView;
 	}
+	private void logicControl() {
+		//HasilPencarianKosAdapter adapter = new HasilPencarianKosAdapter(context, resource, listOfDataKos)
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//				Fragment fragment = new DetailDataSapiFragment(mActivity, mContext, mData_SapiList.get(position));
+//				if (fragment != null) {
+//					General.replaceFragmentAddBackStack(fragment,getFragmentManager());
+//				}
+			}
+		});
+	}
+
+//	public HasilCariKosFragment(Context context) {
+//		mContext = context;
+//		db = new DatabaseHelper(mContext);
+//		sessionManager = new SessionManager(mContext);
+//	}
 
 //	@Override
-//	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//			Bundle savedInstanceState) {
-//		View rootView = inflater.inflate(
-//				R.layout.list_view_fragment_cari_kos, container, false);
-//		setLayout(inflater, rootView);
-//		return rootView;
+//	public void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		activity = (Activity) mContext;
+//		 actioncBar = activity.getActionBar();
+//		// actioncBar.setHomeButtonEnabled(true);
+//		 actioncBar.setDisplayHomeAsUpEnabled(true);
+//		// setHasOptionsMenu(true);
 //	}
 
 //	private void setLayout(LayoutInflater inflater, View resultView) {
@@ -79,19 +91,18 @@ public class HasilCariKosFragment extends Fragment {
 //		}
 //		return super.onOptionsItemSelected(item);
 //	}
-	@Override
-	public void setInitialSavedState(SavedState state) {
-		super.setInitialSavedState(state);
-	}
-	
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-	}
-
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-	}
-
+//	@Override
+//	public void setInitialSavedState(SavedState state) {
+//		super.setInitialSavedState(state);
+//	}
+//	
+//	@Override
+//	public void onActivityCreated(Bundle savedInstanceState) {
+//		super.onActivityCreated(savedInstanceState);
+//	}
+//
+//	@Override
+//	public void onSaveInstanceState(Bundle outState) {
+//		super.onSaveInstanceState(outState);
+//	}
 }
